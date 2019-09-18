1391d35be342ce7762e8ad46d7570bbd4d764251
Manik Hosen <rahathossenmanik@gmail.com>
2019-09-18 15:17:39 +0600

    Cross Correlation Update

--- CrossCorrelation.m
+++ CrossCorrelation.m
@@ -1,35 +1,43 @@
 clc;
-clear;
+clear all;
+close all;
 
-x = [2, -1, 3, 7, 1, 2, -3];
-y = [1, -1, 2, -2, 4, 1, -2, 5];
-
-
-lag = -6:1:7;
-
-N = size(x,2);
-M = size(y,2);
-result = zeros(1, N + M - 1 );
-len = size(result,2);
-
-for m = 1 : len  
-   arg = (m - N); 
-
-   if(arg < 0)
-       negativeCondition = 1;
-       limit = N + arg;
-   else
-       negativeCondition = 0;
-       limit = N - arg;
-   end
-
-   for n = 1:limit
-           if(negativeCondition == 0)
-            result(m) = result(m) + x(arg + n) + y(n);
-           else
-            result(m) = result(m) + x(n) + y(n - arg);
-           end
-   end
-end
-
-stem(lag, result)
+x = input('Enter values of x(n): ');
+zerox = input('Zero Position of x: ');
+h = input('Enter values of h(n): ');
+zeroh = input('Zero Position of h: ');
+k1 = -(length(h)-1);
+k2 = length(x)+length(h)-1;
+kstart = 1;
+kend = k2 - k1 + 1;
+xk = zeros(kstart,kend);
+hk = zeros(kstart,kend);
+for i=1:length(x)
+  xk(-k1+i-zerox+1) = x(i);
+endfor
+for i=1:length(h)
+  hk(i+zeroh-1) = h(i);
+endfor
+shift = 0;
+for j=1:k2-zeroh+1+1
+  X(shift+1) = 0;
+  for i=1:length(h)+zeroh-1
+    X(shift+1) = X(shift+1) + hk(i)*xk(i+shift);
+  endfor
+  shift = shift+1;
+endfor
+disp('Value of x(k): ');
+disp(xk);
+disp('Value of h(-k): ');
+disp(hk);
+disp('Convolution Sum: ');
+disp(X);
+subplot(3,1,1);
+stem(1:length(xk),xk);
+title('x(n)');
+subplot(3,1,2);
+stem(1:length(hk),hk);
+title('h(n)');
+subplot(3,1,3);
+stem(1:length(X),X);
+title('X(m)');
